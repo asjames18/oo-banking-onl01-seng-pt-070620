c00c12d6ee43b363b122d3973d4b64dd845f3ef5
Antonio James <asjames18@gmail.com>
2020-08-04 18:55:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class BankAccount
+
+  attr_accessor :status, :balance
+  attr_reader :name
+  def initialize (bank)
+    @bank = bank
+    @name = "Avi"
+    @balance = 1000
+    @status = "open"
+
+  end
+
+  def deposit(money)
+    money = 2000
+
+  end
+
+  def display_balance
+    display_balance = "Your balance is $#{balance}."
+    binding.pry
+
+  end
+
+  def valid?
+   if @status = "open" && balance > 0
+   true
+ else
+   false
+ end
+  end
+
+def close_account
+  # close_account << "closed"
+binding.pry
+end
 
 end
