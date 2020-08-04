dc62893358c132a864eb1a31cbc424045e61e1b1
Antonio James <asjames18@gmail.com>
2020-08-04 19:15:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -19,7 +19,7 @@ class BankAccount
     display_balance = "Your balance is $#{balance}."
   end
   def valid?
-   if status == "open" && balance > 0
+   if @status == "open" && @balance > 0
    true
   else
     false
