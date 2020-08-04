b1aa9169ada8f6ad94d811a2f38aba7d5513117a
Antonio James <asjames18@gmail.com>
2020-08-04 19:22:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,10 @@ class Transfer
 
   attr_accessor :sender, :reciever, :amount
 
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @amount = amount
+    @reciever = reciever
+
 
 end
